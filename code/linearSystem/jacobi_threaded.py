22f513a2b5c2bc385bce6a0451a3529f5ee29bc6
BarghestEPL <BarghestEPL@gmail.com>
2020-01-02 14:47:09 +0100

    jacobi multi-threaded

--- code/linearSystem/jacobi_threaded.py
+++ code/linearSystem/jacobi_threaded.py
@@ -1,11 +1,13 @@
 # Implementation from Barghest
 import numpy as np
+from threading import Thread
 
 
 # Note: Since jacobi's algorithm is iterative
 # diminish the tolerance to get stricter results ;)
-def jacobi(a, tol=0.1e-8):
+def jacobi(a, n_threads, tol=0.1e-9):
     """
+    :param n_threads: numbers of threads desired to compute the row of x
     :param a: [A/b] -> coefficient matrix of A which last column is the independent term b / A -> SDD matrix
     :param tol: float -> the maximum distance between x and x^-1 such as distance(x, x^-1) < tolerance
     :return: ndarray: the closest vector 'x' of a @ x = b that respect the tolerance
@@ -17,14 +19,21 @@ def jacobi(a, tol=0.1e-8):
 
     n = len(a)
     x, prev = np.zeros(n), np.ones(n)
-    while distance(x, prev) > tol:
-        prev = x.copy()
-        for i in range(n):
+
+    def compute(i):
+        for i in range(i, n, i + 1):
             sigma = 0
             for j in range(n):
                 if i != j:
                     sigma += a[i, j] * x[j]
             x[i] = (1 / a[i, i]) * (a[i, -1] - sigma)
+
+    while distance(x, prev) > tol:
+        prev, threads = x.copy(), [Thread(target=compute, args=(i,), daemon=True) for i in range(n_threads)]
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
     return x
 
 
@@ -33,4 +42,4 @@ A = np.array([[2, -1, 0, -3],
               [0, -1, 2, 4]], dtype='float')
 
 # Ax = b -> { b = (-3, -1, 4), x = (-1.75, -0.5, 1.75) }
-print(jacobi(A))
+print(jacobi(A, 2))
