d561e20ebb99a8d7238f17fd2e114fda82228c19
Barghest-epl <barghestepl@gmail.com>
2020-01-05 11:42:38 +0100

    'checked'

--- code/initialValueProblem/euler_book.py
+++ code/initialValueProblem/euler_book.py
@@ -29,10 +29,11 @@ y = np.array([0.0, 1.0])
 h = 0.05
 
 X, Y = euler_book(F, x, y, xStop, h)
+print(X, Y)
 yExact = 100.0 * X - 5.0 * X ** 2 + 990.0 * (np.exp(-0.1 * X) - 1.0)
 
 # a, a = 0 is a constant as we know f(a) = 0 and f'(a) = 1 ->  y is the numpy array containing those value
-# Y[:, 1] : Y[:, 0] -> [f(x) for x in X] where f is unknown, Y[:, n] -> [f'prime at order n'(x) for x in X]
+# Y[:, 1] : Y[:, 0] -> [~f(x) for x in X] where f is unknown, Y[:, n] -> [~f'prime at order n'(x) for x in X]
 plt.plot(X, yExact, '-', X, Y[:, 0], 'o')
 plt.grid(True)
 plt.title('slide on IVP example 2')
